414406f0c57ca7b46993cc4335370573cf043054
Yuki Suwa <suwa.yuki@classmethod.jp>
2019-10-02 05:08:47 +0900

    [add] First commit.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="!signedIn">
+      <amplify-authenticator :authConfig="authConfig"></amplify-authenticator>
+    </div>
+    <div v-if="signedIn">
+      <h3>Signed in!</h3>
+      <amplify-sign-out></amplify-sign-out>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Auth } from 'aws-amplify'
+import { AmplifyEventBus } from 'aws-amplify-vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      signedIn: false,
+      authConfig: {
+        signUpConfig: {
+          header: 'My Customized Sign Up',
+          hideAllDefaults: true,
+          defaultCountryCode: '81',
+          signUpFields: [
+            {
+              label: 'Email',
+              key: 'username',
+              required: true,
+              displayOrder: 1,
+              type: 'string',
+              signUpWith: true
+            },
+            {
+              label: 'Password',
+              key: 'password',
+              required: true,
+              displayOrder: 2,
+              type: 'string',
+              signUpWith: true
+            }
+          ]
+        }
+      }
+    }
+  },
+  async beforeCreate() {
+    try {
+      await Auth.currentAuthenticatedUser()
+      this.signedIn = true
+    } catch {
+      this.signedIn = false
+    }
+    AmplifyEventBus.$on('authState', info => {
+      if (info === 'signedIn') {
+        this.signedIn = true
+      } else {
+        this.signedIn = false
+      }
+    }); 
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Amplify, * as AmplifyModules from 'aws-amplify'
+import { AmplifyPlugin } from 'aws-amplify-vue'
+import awsconfig from './aws-exports'
+Amplify.configure(awsconfig)
 
 Vue.config.productionTip = false
+Vue.use(AmplifyPlugin, AmplifyModules)
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
